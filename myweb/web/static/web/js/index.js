d2f280af85f4e466bace52a503f488525d6f6f3a
zhaohe <zhaohe@heqiauto.com>
2017-09-26 11:23:47 +0800

    数据修正

--- myweb/web/static/web/js/index.js
+++ myweb/web/static/web/js/index.js
@@ -1,5 +1,5 @@
 /**
- * Created by 疯小埋 on 17/9/14.
+ * Created by fengxiaomai on 17/9/14.
  */
 
 $(document).ready(function (){
@@ -9,3 +9,57 @@ $(document).ready(function (){
         }, 500);
     });
 });
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'hello aaa'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+});
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: '页面加载于' + new Date().toLocaleString()
+    }
+});
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+});
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+       todos: [
+           {text: 'wqfeqwf'},
+           {text: 'wqrqwerq'},
+           {text: 'qwrwvasdcv'}
+       ]
+    }
+});
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+});
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        groceryList: [
+            {id: 0, text: 'qq'},
+            {id: 1, text: 'ww'},
+            {id: 2, text: 'ee'}
+        ]
+    }
+});
+
